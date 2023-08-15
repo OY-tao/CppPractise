e1c156fb753ce6981368dff03ea0f02bef297019
M954 <xu770310@163.com>
2023-08-15 15:22:30 +0800

    first commit

---
+++ ChatRoom/Client.hpp
@@ -0,0 +1,35 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+#include "Common.hpp"
+
+const char EXIT[] = "exit";
+
+class Client {
+public:
+    Client();
+
+    ~Client();
+
+    void Start();
+
+    void Init();
+
+    void Close();
+    
+private:
+    struct sockaddr_in serverAddr_;
+
+    int socketFd_;
+
+    int epollFd_;
+
+    int pid_;
+
+    int pipeFd_[2];
+
+    bool isClientWork;
+
+    char message_[BUFF_SIZE];
+};
+
+#endif //CLIENT_HPP
